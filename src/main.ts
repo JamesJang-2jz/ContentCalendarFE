348a34f7faa986b4ef39ab4b3cf0753eef9f6db6
JamesJang-2jz <jhjx92@gmail.com>
2023-06-29 00:20:20 -0400

    front end renders json to html to view in browser

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
 fetch('http://localhost:8080/api/content')
-.then((response: Response) => response.json())
-.then((data: any) => console.log(data));
-
+  .then((response: Response) => response.json())
+  .then((data: any) => {
+    const rootElement = document.getElementById('root');
+    const preElement = document.createElement('pre');
+    preElement.textContent = JSON.stringify(data, null, 2);
+    rootElement?.appendChild(preElement);
+  })
+  .catch((error: any) => {
+    console.error('Error fetching data:', error);
+  });
